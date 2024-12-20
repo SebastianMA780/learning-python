624e2647a081193c0aed8a7b8e78cf8d110851fd
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2024-12-20 09:51:03 -0500

    adds python variables and data types files

---
+++ src/data-types.py
@@ -0,0 +1,6 @@
+string = "New string"
+number = 10
+
+print(type(string))
+print(f"type string: {string} and type number: {number}")
+print("type string %s and type number %d" % (string,number))
---
+++ src/variables.py
@@ -0,0 +1,18 @@
+# This is a module to demonstrate variables in Python """
+
+# Variables are used to store data values in Python. A variable is created the moment 
+# you first assign a value to it and is assigned using the assignment operator (=) and the name. You cant use reserved keywords as variable names.
+
+
+name = "python"
+print(name)
+
+# You can overwrite the value of a variable , remember python is organized from top to bottom and left to right in execution
+name = "python 3"
+print(name)
+
+#underscores are allowed in variable names
+_name = "python new"
+print(_name)
+name_python = "python new"
+
