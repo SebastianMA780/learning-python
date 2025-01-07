62125d5da7c79f6fb922d1b68f42f18f32dc4874
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2025-01-07 16:08:07 -0500

    adds function tips

--- src/11_functions.py
+++ src/11_functions.py
@@ -8,3 +8,13 @@ def add(a, b):
 
 result = add(2, 3)
 print(result) # 5
+
+
+#### 
+
+def find_volume(length=1, width=1, height=1):
+		return length * width * height , height
+
+result = find_volume(width=10)
+
+print(result) # returns a tuple (10, 1) 
