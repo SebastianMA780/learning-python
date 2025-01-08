0efb802418e75670379c6c434b3aad18e5919849
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2025-01-08 09:35:56 -0500

    adds iterator tips

--- src/10_iterators.py
+++ src/10_iterators.py
@@ -1,3 +1,8 @@
+# An Iterator is an object that can be iterated upon.
+# But with the difference that it will be triggered manually. This is done through the next() function.
+# The next() function will keep calling the next item until it reaches the end.
+# This has the advantage of being more memory efficient, because it doesn't load all the items into memory at once.
+
 my_list = [1, 2, 3, 4, 5]
 my_iter = iter(my_list)
 print(next(my_iter))  # 1
